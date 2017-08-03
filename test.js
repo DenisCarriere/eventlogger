2aea6a03f2aebe6d5c727413d9a792736c6dd0be
Denis <carriere.denis@gmail.com>
2017-08-02 20:16:09 -0400

    Add travis Testing

--- test.js
+++ test.js
@@ -5,40 +5,43 @@ const test = require('tape')
 const moment = require('moment')
 const EventLogger = require('./')
 
-test('default values', t => {
-  const logPath = path.join(os.homedir(), 'Library', 'Logs', 'Node.js')
-  const log = new EventLogger()
-  t.equal(log.source, 'Node.js', 'source')
-  t.equal(log.eventLog, 'APPLICATION', 'eventLog')
-  t.equal(log.logPath, logPath, 'logPath')
-  t.end()
-})
+switch (os.platform()) {
+  case 'darwin':
+    test('darwin -- default values', t => {
+      const logPath = path.join(os.homedir(), 'Library', 'Logs', 'Node.js')
+      const log = new EventLogger()
+      t.equal(log.source, 'Node.js', 'source')
+      t.equal(log.eventLog, 'APPLICATION', 'eventLog')
+      t.equal(log.logPath, logPath, 'logPath')
+      t.end()
+    })
 
-test('custom values', t => {
-  const logPath = '~/Library/Logs/Foo'
-  const log = new EventLogger({
-    logPath,
-    source: 'Hello World',
-    eventLog: 'SYSTEM'
-  })
-  t.equal(log.source, 'Hello World', 'source')
-  t.equal(log.eventLog, 'SYSTEM', 'eventLog')
-  t.equal(log.logPath, path.join(os.homedir(), 'Library', 'Logs', 'Foo'), 'logPath')
-  t.end()
-})
+    test('darwin -- custom values', t => {
+      const logPath = '~/Library/Logs/Foo'
+      const log = new EventLogger({
+        logPath,
+        source: 'Hello World',
+        eventLog: 'SYSTEM'
+      })
+      t.equal(log.source, 'Hello World', 'source')
+      t.equal(log.eventLog, 'SYSTEM', 'eventLog')
+      t.equal(log.logPath, path.join(os.homedir(), 'Library', 'Logs', 'Foo'), 'logPath')
+      t.end()
+    })
 
-test('write', t => {
-  const source = 'EventLogger'
-  const date = moment().format('YYYY-MM-DD')
-  const logPath = path.join(os.homedir(), 'Library', 'Logs', source)
-  const logFilePath = path.join(logPath, `${source}_${date}.log`)
-  fs.removeSync(logPath)
-  const log = new EventLogger(source)
+    test('darwin -- write', t => {
+      const source = 'EventLogger'
+      const date = moment().format('YYYY-MM-DD')
+      const logPath = path.join(os.homedir(), 'Library', 'Logs', source)
+      const logFilePath = path.join(logPath, `${source}_${date}.log`)
+      fs.removeSync(logPath)
+      const log = new EventLogger(source)
 
-  log.warn('Basic information.')
-  log.info('Watch out!')
-  log.error('Something went wrong.')
+      log.warn('Basic information.')
+      log.info('Watch out!')
+      log.error('Something went wrong.')
 
-  t.equal(fs.readFileSync(logFilePath).byteLength, 173, 'saved log')
-  t.end()
-})
+      t.equal(fs.readFileSync(logFilePath).byteLength, 173, 'saved log')
+      t.end()
+    })
+}
