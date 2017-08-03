07df28435ba2c450954c5084113beb88c629b573
Denis <carriere.denis@gmail.com>
2017-08-02 20:26:55 -0400

    Convert library to ES5

--- darwin/index.js
+++ darwin/index.js
@@ -1,125 +1,125 @@
-const fs = require('fs')
-const os = require('os')
-const path = require('path')
-const mkdirp = require('mkdirp')
-const moment = require('moment')
+var fs = require('fs')
+var os = require('os')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var moment = require('moment')
 
-const EVENT_TYPES = ['APPLICATION', 'SYSTEM']
-const MESSAGE_TYPES = ['ERROR', 'WARNING', 'INFORMATION', 'SUCCESSAUDIT', 'FAILUREAUDIT']
+var EVENT_TYPES = ['APPLICATION', 'SYSTEM']
+var MESSAGE_TYPES = ['ERROR', 'WARNING', 'INFORMATION', 'SUCCESSAUDIT', 'FAILUREAUDIT']
 
 /**
- * EventLogger
+ * EventLogger Constructor
+ *
+ * @param {Object|string} [config={}] Configuration
+ * @param {string} [config.source='Node.js'] Source
+ * @param {string} [config.eventLog='APPLICATION'] Event Log
+ * @param {string} [config.logPath='~/Library/Logs/Node.js'] Log Path
  */
-class EventLogger {
-  /**
-   * EventLogger Constructor
-   *
-   * @param {Object|string} [config={}] Configuration
-   * @param {string} [config.source='Node.js'] Source
-   * @param {string} [config.eventLog='APPLICATION'] Event Log
-   * @param {string} [config.logPath='~/Library/Logs/Node.js'] Log Path
-   */
-  constructor (config = {}) {
-    if (typeof config === 'string') config = {source: config}
-    const eventLog = config.eventLog ? config.eventLog.toUpperCase() : 'APPLICATION'
-    this.source = config.source || 'Node.js'
-    this.eventLog = EVENT_TYPES.indexOf(eventLog) >= 0 ? eventLog : 'APPLICATION'
-    this.logPath = config.logPath || path.join(os.homedir(), 'Library', 'Logs', this.source)
-    this.logPath = this.logPath.replace(/~/, os.homedir())
-    mkdirp.sync(this.logPath)
-  }
+function EventLogger (config) {
+  config = config || {}
+  if (typeof config === 'string') config = {source: config}
+  var eventLog = config.eventLog ? config.eventLog.toUpperCase() : 'APPLICATION'
+  this.source = config.source || 'Node.js'
+  this.eventLog = EVENT_TYPES.indexOf(eventLog) >= 0 ? eventLog : 'APPLICATION'
+  this.logPath = config.logPath || path.join(os.homedir(), 'Library', 'Logs', this.source)
+  this.logPath = this.logPath.replace(/~/, os.homedir())
+  mkdirp.sync(this.logPath)
+}
 
-  /**
-   * Write
-   *
-   * @param {string} messageType Message Type
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   * @example
-   * //= 2016-03-15T23:25:23Z [6419] INFO file-descriptors (nofiles) new hard limit is -1, new soft limit is 1024
-   */
-  write (messageType, message, code, callback) {
-    // Handle Code
-    code = code || 1000
-    code = (typeof code === 'number') ? code : 1000
+/**
+ * Write
+ *
+ * @param {string} messageType Message Type
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ * @example
+ * //= 2016-03-15T23:25:23Z [6419] INFO file-descriptors (nofiles) new hard limit is -1, new soft limit is 1024
+ */
+EventLogger.prototype.write = function (messageType, message, code, callback) {
+  // Handle Code
+  code = code || 1000
+  code = (typeof code === 'number') ? code : 1000
 
-    // Handle Message
-    messageType = (messageType) ? messageType.trim().toUpperCase() : 'INFORMATION'
-    messageType = MESSAGE_TYPES.indexOf(messageType) >= 0 ? messageType : 'INFORMATION'
+  // Handle Message
+  messageType = (messageType) ? messageType.trim().toUpperCase() : 'INFORMATION'
+  messageType = MESSAGE_TYPES.indexOf(messageType) >= 0 ? messageType : 'INFORMATION'
 
-    // Handle Dates
-    const time = moment().toISOString()
-    const date = moment().format('YYYY-MM-DD')
+  // Handle Dates
+  var time = moment().toISOString()
+  var date = moment().format('YYYY-MM-DD')
 
-    // Save Log
-    const outputPath = path.join(this.logPath, `${this.source}_${date}.log`)
-    let outputMessage = ''
-    if (fs.existsSync(outputPath)) outputMessage += '\n'
-    outputMessage += `${time} [${code}] ${messageType} ${message}`
-    fs.appendFileSync(outputPath, outputMessage)
-  }
+  // Save Log
+  var outputPath = path.join(this.logPath, this.source + '_' + date + '.log')
+  var outputMessage = ''
+  if (fs.existsSync(outputPath)) outputMessage += '\n'
+  outputMessage += time + ' [' + code + '] ' + messageType + ' ' + message
+  fs.appendFileSync(outputPath, outputMessage)
+}
 
-  /**
-   * Warning
-   *
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   */
-  warn (message, code, callback) {
-    this.write('WARNING', message, code, callback)
-  }
-  warning (message, code, callback) {
-    this.warn(message, code, callback)
-  }
+/**
+ * Warning
+ *
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ */
+EventLogger.prototype.warn = function (message, code, callback) {
+  this.write('WARNING', message, code, callback)
+}
+EventLogger.prototype.warning = function (message, code, callback) {
+  this.warn(message, code, callback)
+}
 
-  /**
-   * Information
-   *
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   */
-  info (message, code, callback) {
-    this.write('INFORMATION', message, code, callback)
-  }
-  information (message, code, callback) {
-    this.info(message, code, callback)
-  }
+/**
+ * Information
+ *
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ */
+EventLogger.prototype.info = function (message, code, callback) {
+  this.write('INFORMATION', message, code, callback)
+}
+EventLogger.prototype.information = function (message, code, callback) {
+  this.info(message, code, callback)
+}
 
-  /**
-   * Error
-   *
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   */
-  error (message, code, callback) {
-    this.write('ERROR', message, code, callback)
-  }
+/**
+ * Error
+ *
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ */
+EventLogger.prototype.error = function (message, code, callback) {
+  this.write('ERROR', message, code, callback)
+}
 
-  /**
-   * Audit Success
-   *
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   */
-  auditSuccess (message, code, callback) {
-    this.write('SUCCESSAUDIT', message, code, callback)
-  }
+/**
+ * Audit Success
+ *
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ */
+EventLogger.prototype.auditSuccess = function (message, code, callback) {
+  this.write('SUCCESSAUDIT', message, code, callback)
+}
 
-  /**
-   * Audit Failure
-   *
-   * @param {string} message Message
-   * @param {number} [code=1000] Code
-   * @param {Function} [callback] Callback
-   */
-  auditFailure (message, code, callback) {
-    this.write('FAILUREAUDIT', message, code, callback)
-  }
+/**
+ * Audit Failure
+ *
+ * @param {string} message Message
+ * @param {number} [code=1000] Code
+ * @param {Function} [callback] Callback
+ */
+EventLogger.prototype.auditFailure = function (message, code, callback) {
+  this.write('FAILUREAUDIT', message, code, callback)
+}
+
+EventLogger.prototype.toString = function () {
+  return 'EventLogger("' + this.source + '")'
 }
 
 module.exports = EventLogger
